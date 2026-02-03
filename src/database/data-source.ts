3cf319a98833bc03c1be2b0fcac172a6e26cc2f9
Christian sagastegui <csagastegui59@gmail.com>
2026-02-03 16:38:44 -0500

    fix: fix database deployment

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is missing');
+}
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  port: Number(process.env.DB_PORT ?? 5432),
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
