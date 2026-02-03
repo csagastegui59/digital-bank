aabd96d8bf4e34b5d87066a64110bae75753db6a
Christian sagastegui <csagastegui59@gmail.com>
2026-02-03 16:27:43 -0500

    feature: change mysql to postgres db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,11 @@ import { TransactionsModule } from './transactions/transactions.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT ?? 3306),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
       autoLoadEntities: true,
       synchronize: false,
-      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
     }),
     AuthModule,
     UsersModule,
@@ -24,4 +20,4 @@ import { TransactionsModule } from './transactions/transactions.module';
     TransactionsModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,14 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
+  url: process.env.DATABASE_URL,
   port: Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
   logging: false,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 });
